fdac13565b0b1f411ba09625353353fd062e8251
Merajul Islam <meraj.exe@gmail.com>
2023-03-02 23:04:50 +0600

    fetch data complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-console.log('connected');
+// fetch all data from api
+const fetchAllDataFromApi = () => {
+  fetch('https://openapi.programming-hero.com/api/ai/tools')
+    .then(res => res.json())
+    .then(data => showElementsToUi(data.data.tools));
+};
+
+const showElementsToUi = aitools => {};
+
+fetchAllDataFromApi();
